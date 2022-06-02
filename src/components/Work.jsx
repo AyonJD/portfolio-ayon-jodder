42262b1e3d2defdd85690b6992724c238b0d8ef4
Ayon <ayonjodder177@gmail.com>
2022-06-02 18:46:44 +0600

    Projects updated

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,13 +4,13 @@ import realEstate from '../assets/realestate.jpg';
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#022C43]'>
+    <div name='work' className='w-full my-28 md:h-screen text-gray-300 bg-[#022C43]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#F7F700]'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#F7F700] my-name bg-white'>
             Work
           </p>
-          <p className='py-6'>// Check out some of my recent work</p>
+          <p className='py-6 my-name bg-white'>// Check out some of my recent work</p>
         </div>
 
 {/* Container */}
