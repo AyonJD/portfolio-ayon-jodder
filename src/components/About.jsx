02a4b37b77d4082817e8b563029070dfbc4f447a
Ayon <ayonjodder177@gmail.com>
2022-06-02 17:43:27 +0600

    About section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl font-bold inline border-b-4 border-[#F7F700]'>
+            <p className='my-name bg-white text-4xl font-bold inline border-b-4 border-[#F7F700]'>
               About
             </p>
           </div>
@@ -14,14 +14,10 @@ const About = () => {
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Clint, nice to meet you. Please take a look around.</p>
+              <p className='my-name bg-white'>Hi. I'm <span className='text-[#F7F700] my-name bg-[#F7F700]'>Ayon</span>, It's great to finally meet you. Please look around.</p>
             </div>
             <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. I specialize in creating software
-              for clients ranging from individuals and small-businesses all the
-              way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p>  
+              <p className='my-name bg-white text-[#a6b2d6]'>I'm passionate about creating high-quality web applications that benefit individuals around me. I specialize in developing software for a wide range of clients, from individuals to small enterprises to huge corporations. What would you do if you had access to a software specialist at any time?</p>  
             </div>
           </div>
       </div>
